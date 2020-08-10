64dc3e4a5b25ee3f6a6b548f66cd0214201000e2
melanie <melanieduah@gmail.com>
2020-08-10 00:57:46 -0500

    build

---
+++ build/precache-manifest.c0ea32d290d0379ffd89917ddf643a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c500ed904976b6b6d1ef15f81107df43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251026ded75f7f3540a3",
+    "url": "/static/js/2.459740e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.459740e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d86df88a5a63c4dccf7",
+    "url": "/static/js/main.7b4e055d.chunk.js"
+  },
+  {
+    "revision": "65bdfdef76476d120aac",
+    "url": "/static/js/runtime-main.dfda8a66.js"
+  }
+]);
