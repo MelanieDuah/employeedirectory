0f2c77fb3bf6cc24ae826299a01062817cdf2a84
melanie <melanieduah@gmail.com>
2020-08-11 22:15:27 -0500

    rebuild

---
+++ build/precache-manifest.2b407a4b7ae6103a584a9fbd94c7eecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376a6e643efb79349222d957516d6135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251026ded75f7f3540a3",
+    "url": "/static/js/2.459740e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.459740e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e61e2c63af8af8bdf7",
+    "url": "/static/js/main.f47edc11.chunk.js"
+  },
+  {
+    "revision": "65bdfdef76476d120aac",
+    "url": "/static/js/runtime-main.dfda8a66.js"
+  }
+]);
